ec16435c53bc4b8bcdf8803e36cccb7f3ad42119
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-01-15 08:22:15 -0500

    Use sh instead of bash
    Alpine doesn’t have bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Fail the job if there are any failures below.
 set -e
 
-if [ "$1" == "npm" ]; then
+if [ "$1" = "npm" ]; then
   test -f "package-lock.json" || (echo "Error! Expected a package-lock.json file but none was found." && exit 1)
   test ! -f "yarn.lock" || (echo "Error! Extraneous yarn.lock file was found." && exit 1)
-elif [ "$1" == "yarn" ]; then
+elif [ "$1" = "yarn" ]; then
   test -f "yarn.lock" || (echo "Error! Expected a yarn.lock file but none was found." && exit 1)
   test ! -f "package-lock.json" || (echo "Error! Extraneous package-lock.json file was found." && exit 1)
 else
