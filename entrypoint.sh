3dbed0df720de64b6525e5290e6f0dd43316eaea
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-01-15 11:34:53 -0500

    Test the JavaScript lockfile check (#5)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Fail the job if there are any failures below.
 set -e
 
-if [ "$1" == "npm" ]; then
-  test -f "package-lock.json" || (echo "Error! Expected a package-lock.json file but none was found." && exit 1)
-  test ! -f "yarn.lock" || (echo "Error! Extraneous yarn.lock file was found." && exit 1)
-elif [ "$1" == "yarn" ]; then
-  test -f "yarn.lock" || (echo "Error! Expected a yarn.lock file but none was found." && exit 1)
-  test ! -f "package-lock.json" || (echo "Error! Extraneous package-lock.json file was found." && exit 1)
+# Function that sets GH Action output and exits w/non-zero code.
+emit_error()
+{
+  # Our action uses the lockfile-check-result set below as part of its output.
+  # See `outputs` in the action.yml file in this repo.
+  # Ref: https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#setting-an-output-parameter
+  echo "::set-output name=lockfile-check-result::failure" && exit 1
+}
+
+if [ "$1" = "npm" ]; then
+  test -f "package-lock.json" || (echo "Error! Expected a package-lock.json file but none was found." && emit_error)
+  test ! -f "yarn.lock" || (echo "Error! Extraneous yarn.lock file was found." && emit_error)
+  echo "::set-output name=lockfile-check-result::success"
+elif [ "$1" = "yarn" ]; then
+  test -f "yarn.lock" || (echo "Error! Expected a yarn.lock file but none was found." && emit_error)
+  test ! -f "package-lock.json" || (echo "Error! Extraneous package-lock.json file was found." && emit_error)
+  echo "::set-output name=lockfile-check-result::success"
 else
-  echo "Error! Unknown package manager type: $1" 1>&2 && exit 1
+  echo "Error! Unknown package manager type: $1" 1>&2 && emit_error
 fi
